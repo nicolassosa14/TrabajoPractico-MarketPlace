c0904f78b22cd4e5f32fa8566c8da0c6e424da62
Nicolas <pikachugd68@gmail.com>
2025-09-08 21:59:43 -0300

    Supabase Connection

--- src/supabase/supabase.module.ts
+++ src/supabase/supabase.module.ts
@@ -4,8 +4,8 @@ import {createClient} from '@supabase/supabase-js'
 const SupabaseProvider = {
     provide: 'SUPABASE_CLIENT',
     useFactory: () => {
-        const supabaseUrl = ''
-        const supabaseKey = ''
+        const supabaseUrl = 'https://fcdvvaygoclixkfiolfd.supabase.co'
+        const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZjZHZ2YXlnb2NsaXhrZmlvbGZkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTYxNjgzNjMsImV4cCI6MjA3MTc0NDM2M30.7WPAXzA8WxkxfHSX-ckZajk7UsLkFrBwa8OhpOSt4TQ'
         return createClient(supabaseUrl, supabaseKey)
     }
 };
